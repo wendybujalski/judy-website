33d8bb2f4e27a291539bff0c7a47ed0658d7aadb
wendybujalski <wendydbujalski@gmail.com>
2022-09-11 17:30:22 -0400

    linted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,26 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="wrapper">
-        <nav id="sidebar" class="">
-            <div class="sidebar-header">
-                <h3>Judy Giera</h3>
-                <p>Not So Fine Art</p>
-            </div>
+  <div class="wrapper">
+    <nav id="sidebar" class="">
+      <div class="sidebar-header">
+        <h3>Judy Giera</h3>
+        <p>Not So Fine Art</p>
+      </div>
 
-            <ul class="list-unstyled components">
-                <li class="active">
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </nav>
+      <ul class="list-unstyled components">
+        <li class="active">
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
 
-        <div id="content">
-          stuff goes here
-        </div>
-    </div>
+    <div id="content">stuff goes here</div>
+  </div>
 </template>
